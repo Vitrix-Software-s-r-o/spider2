add5e9e67f240d315a7cb1fcfdf0ed78e5a775fd
Jan Hruby <jan@vitrix.cz>
2025-06-13 22:23:31 +0200

    more tests

--- spider2/tests/multipart_parser_tests.cpp
+++ spider2/tests/multipart_parser_tests.cpp
@@ -35,7 +35,6 @@ static_assert(multipart_event_handler<test_message_handler>,
 
 TEST_CASE("test simple multipart_parser", "[multipart_parser]")
 {
-
    auto handler = test_message_handler{};
    auto parser = multipart_message_parser{"boundary", handler};
 
@@ -53,4 +52,74 @@ TEST_CASE("test simple multipart_parser", "[multipart_parser]")
    REQUIRE(handler.parts.size() == 1);
    REQUIRE(handler.parts[0].first["Content-Disposition"] == "form-data; name=\"file\"; filename=\"test.txt\"");
    REQUIRE(handler.parts[0].second == "Hello World\r\n");
+}
+
+TEST_CASE("test two parts multipart_parser", "[multipart_parser]")
+{
+   auto handler = test_message_handler{};
+   auto parser = multipart_message_parser{"boundary", handler};
+
+   std::string_view data =
+       "--boundary\r\n"
+"Content-Disposition: form-data; name=\"file\"; filename=\"test.txt\"\r\n"
+"\r\n"
+"Hello World"
+"--boundary\r\n"
+"Content-Disposition: form-data; name=\"file2\"; filename=\"test2.txt\"\r\n"
+"\r\n"
+"Hello Again\r\n"
+"--boundary--\r\n";
+
+
+   error_code ec;
+   parser.on_data(std::span<const std::byte>{reinterpret_cast<const std::byte *>(data.data()), data.size()}, ec);
+   parser.on_finish(ec);
+
+   REQUIRE(!ec);
+   REQUIRE(handler.finish_count == 1);
+   REQUIRE(handler.parts_count == 2);
+   REQUIRE(handler.parts.size() == 2);
+   REQUIRE(handler.parts[0].first["Content-Disposition"] == "form-data; name=\"file\"; filename=\"test.txt\"");
+   REQUIRE(handler.parts[0].second == "Hello World");
+   REQUIRE(handler.parts[1].first["Content-Disposition"] == "form-data; name=\"file2\"; filename=\"test2.txt\"");
+   REQUIRE(handler.parts[1].second == "Hello Again\r\n");
+}
+
+TEST_CASE("test two parts multipart_parser - by single character", "[multipart_parser]")
+{
+   auto handler = test_message_handler{};
+   auto parser = multipart_message_parser{"boundary", handler};
+
+   std::string_view data =
+       "--boundary\r\n"
+"Content-Disposition: form-data; name=\"file\"; filename=\"test.txt\"\r\n"
+"\r\n"
+"Hello World"
+"--boundary\r\n"
+"Content-Disposition: form-data; name=\"file2\"; filename=\"test2.txt\"\r\n"
+"\r\n"
+"Hello Again\r\n"
+"--boundary--\r\n";
+
+
+   error_code ec;
+
+   for (const char & i : data)
+   {
+      parser.on_data(std::span<const std::byte>{reinterpret_cast<const std::byte *>(&i), 1}, ec);
+      if (ec)
+      {
+         break;
+      }
+   }
+   parser.on_finish(ec);
+
+   REQUIRE(!ec);
+   REQUIRE(handler.finish_count == 1);
+   REQUIRE(handler.parts_count == 2);
+   REQUIRE(handler.parts.size() == 2);
+   REQUIRE(handler.parts[0].first["Content-Disposition"] == "form-data; name=\"file\"; filename=\"test.txt\"");
+   REQUIRE(handler.parts[0].second == "Hello World");
+   REQUIRE(handler.parts[1].first["Content-Disposition"] == "form-data; name=\"file2\"; filename=\"test2.txt\"");
+   REQUIRE(handler.parts[1].second == "Hello Again\r\n");
 }
